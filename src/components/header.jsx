709b7398332ea4816c4c50090251627f02a1882b
samuel <samawyck@gmail.com>
2025-04-28 13:49:38 -0500

    Create Header Component

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import "../styles/header.css";
+
+
+function Header() {
+    return (
+        <header>
+            <p>Resume Builder</p>
+            <nav>
+                <a href="#">Templates</a>
+                <a href="#">AI Assistance</a>
+                <a href="#">Support</a>
+            </nav>
+        </header>
+    );
+};
+
+
+export default Header;
