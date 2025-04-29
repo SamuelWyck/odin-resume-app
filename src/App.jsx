63bd752034863360365a3a48614f17f61baa5141
samuel <samawyck@gmail.com>
2025-04-29 12:33:33 -0500

    Install generalForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/header';
+import GeneralForm from './components/generalForm';
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <main>
+        <GeneralForm></GeneralForm>
+      </main>
     </>
   )
 }
