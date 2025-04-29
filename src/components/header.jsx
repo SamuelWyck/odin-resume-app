ea90ea51c01c04fec0e4ee6e922053c6e34bb116
samuel <samawyck@gmail.com>
2025-04-29 09:25:43 -0500

    Create dropdown menu for small screens

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,25 @@
 import "../styles/header.css";
+import menu from "../assets/menu.svg";
 
 
 function Header() {
+
+    function handleClick() {
+        const nav = document.querySelector(".header-menu");
+        nav.classList.toggle("show");
+    };
+
+
     return (
         <header>
             <p>Resume Builder</p>
-            <nav>
+            <button className="menu-btn" onClick={handleClick}><img src={menu} alt="#"/></button>
+            <nav className="header-nav">
+                <a href="#">Templates</a>
+                <a href="#">AI Assistance</a>
+                <a href="#">Support</a>
+            </nav>
+            <nav className="header-menu">
                 <a href="#">Templates</a>
                 <a href="#">AI Assistance</a>
                 <a href="#">Support</a>
